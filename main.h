084c190329870089745ddc120b3a9868a0b7b384
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-24 22:24:36 +0000

    header file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+int _printf(const char *format, ...);
+void print_char(char c);
+void print_string(char *s);
+
+#endif /*MAIN_H*/
