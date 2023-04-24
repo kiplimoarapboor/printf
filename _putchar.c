e61c16456758820575428c11a5d33e1f750e3c77
kiplimoboor <kiplimoboor@hotmail.com>
2023-04-24 22:23:25 +0000

    putchar implementation

--- _putchar.c
+++ _putchar.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- *
+ * _putchar - prints character c
  * @c: The character to print
  *
- * Return: Always (1) Success else
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1,
+ * on error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
